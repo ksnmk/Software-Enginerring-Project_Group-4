4168bec9fa0dca2ebd11a35bff04fff74f9d4218
ksnmk <ksnmk328@gmail.com>
2023-11-01 15:06:58 -0500

    Navigation

---
+++ accommodation_connect/src/components/LandlordNav/LandlordNav.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import "./LandlordNav.css";
+import { useNavigate } from 'react-router-dom';
+
+function LandlordNav() {
+
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    localStorage.clear();
+    navigate('/Login');
+  }
+
+  return (
+    <nav className="navbar">
+      <a href="/landlordpage" className="nav-brand">Accomodation Connect</a>
+      <ul className="nav-menu">
+        <li><a href="/landlordpage">Add Property</a></li>
+        <li><a href="/propertydisplay">Property</a></li>
+        <li><button onClick={handleLogout}>Logout</button></li>
+      </ul>
+    </nav>
+  );
+}
+
+export default LandlordNav;
