35a1d38b19f89ba6bd74676ef21a41830adca549
chiranjeevi557 <chiranjeevinaidu5324@gmail.com>
2023-11-01 15:37:29 -0500

    Updated the SQL file to accomodate the new changes

--- accommodation_connect/project4.sql
+++ accommodation_connect/project4.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 10, 2023 at 06:56 AM
+-- Generation Time: Oct 19, 2023 at 09:20 AM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -33,7 +33,7 @@ CREATE TABLE `landlords` (
   `lastname` varchar(120) NOT NULL,
   `username` varchar(120) NOT NULL,
   `email` varchar(120) NOT NULL,
-  `contact_no` int(10) NOT NULL,
+  `contact_no` bigint(20) NOT NULL,
   `password` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
@@ -42,8 +42,59 @@ CREATE TABLE `landlords` (
 --
 
 INSERT INTO `landlords` (`landlord_id`, `firstname`, `lastname`, `username`, `email`, `contact_no`, `password`) VALUES
-(1, 'sample', 'sample', 'sample', 'sampleuser@gmail.com', 2147483647, 'sampleuser'),
-(2, 'yash', 'yash', 'yash', 'yash@gmail.com', 2147483647, 'yashyash');
+(1, 'sample', 'sample', 'sample', 'sampleuser@gmail.com', 9582314675, 'sampleuser'),
+(2, 'yash', 'yash', 'yash', 'yash@gmail.com', 9885451274, 'yashyash');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `properties`
+--
+
+CREATE TABLE `properties` (
+  `property_id` int(11) NOT NULL,
+  `landlord_id` int(11) NOT NULL,
+  `property_type` varchar(50) NOT NULL,
+  `property_name` varchar(120) NOT NULL,
+  `location` varchar(100) NOT NULL,
+  `rent` decimal(10,2) NOT NULL,
+  `bedrooms` int(11) NOT NULL,
+  `max_members` int(11) NOT NULL,
+  `description` text NOT NULL,
+  `image_path` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `properties`
+--
+
+INSERT INTO `properties` (`property_id`, `landlord_id`, `property_type`, `property_name`, `location`, `rent`, `bedrooms`, `max_members`, `description`, `image_path`) VALUES
+(10, 1, 'apartment', 'Osama Property', 'beside apartment', 35.00, 3, 3, 'good', 'uploads\\image-1697091598102.jpg'),
+(13, 1, 'apartment', 'Yahoo properties', 'beside apartment', 50.00, 2, 3, 'good', 'uploads\\image-1697089864973.jpg'),
+(14, 1, 'Room', 'Osama', 'New Jersey', 600.00, 6, 4, 'Good Atmosphere', 'uploads\\image-1697090024470.jpg');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `reports`
+--
+
+CREATE TABLE `reports` (
+  `report_id` int(11) NOT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  `report_type` varchar(50) DEFAULT NULL,
+  `report_text` text DEFAULT NULL,
+  `reported_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `reports`
+--
+
+INSERT INTO `reports` (`report_id`, `user_id`, `report_type`, `report_text`, `reported_at`) VALUES
+(1, 3, 'Fraud', 'asdasd', '2023-10-12 14:26:19'),
+(2, 3, 'Fraud', 'asd', '2023-10-12 14:26:26'),
+(3, 3, 'Fraud', 'sasd', '2023-10-12 14:49:40');
 
 -- --------------------------------------------------------
 
@@ -57,7 +108,7 @@ CREATE TABLE `users` (
   `lastname` varchar(120) NOT NULL,
   `username` varchar(120) NOT NULL,
   `email` varchar(100) NOT NULL,
-  `contact_no` int(10) NOT NULL,
+  `contact_no` bigint(20) NOT NULL,
   `password` varchar(120) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
@@ -66,8 +117,32 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`user_id`, `firstname`, `lastname`, `username`, `email`, `contact_no`, `password`) VALUES
-(2, 'Ram', 'Raju', 'Ram_raj', 'ram@gmail.com', 2147483647, 'ramaraju'),
-(3, 'asd', 'asd', 'asd', 'asd@gmail.com', 0, 'asdasdasd');
+(2, 'Ram', 'Raju', 'Ram_raj', 'ram@gmail.com', 9685741235, 'ramaraju'),
+(3, 'asd', 'asd', 'asd', 'asd@gmail.com', 9856745124, 'asdasdasd'),
+(6, 'arjun', 'arjun', 'arjun', 'arjun@gmail.com', 9456823147, 'arjunarjun'),
+(7, 'test', 'test', 'test', 'test@gmail.com', 8574965896, 'testtest'),
+(8, 'robo', 'robo', 'robo', 'robo@gmail.com', 9876543212, 'roborobo');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `verification`
+--
+
+CREATE TABLE `verification` (
+  `verification_id` int(11) NOT NULL,
+  `property_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `passport_number` varchar(20) NOT NULL,
+  `us_since_date` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `verification`
+--
+
+INSERT INTO `verification` (`verification_id`, `property_id`, `user_id`, `passport_number`, `us_since_date`) VALUES
+(1, 10, 3, 'asd123asd123', '2022-11-19');
 
 --
 -- Indexes for dumped tables
@@ -80,10 +155,32 @@ ALTER TABLE `landlords`
   ADD PRIMARY KEY (`landlord_id`);
 
 --
+-- Indexes for table `properties`
+--
+ALTER TABLE `properties`
+  ADD PRIMARY KEY (`property_id`),
+  ADD KEY `landlord_id` (`landlord_id`);
+
+--
+-- Indexes for table `reports`
+--
+ALTER TABLE `reports`
+  ADD PRIMARY KEY (`report_id`),
+  ADD KEY `user_id` (`user_id`);
+
+--
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
   ADD PRIMARY KEY (`user_id`);
+
+--
+-- Indexes for table `verification`
+--
+ALTER TABLE `verification`
+  ADD PRIMARY KEY (`verification_id`),
+  ADD KEY `property_id` (`property_id`),
+  ADD KEY `user_id` (`user_id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -96,10 +193,51 @@ ALTER TABLE `landlords`
   MODIFY `landlord_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
+-- AUTO_INCREMENT for table `properties`
+--
+ALTER TABLE `properties`
+  MODIFY `property_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+
+--
+-- AUTO_INCREMENT for table `reports`
+--
+ALTER TABLE `reports`
+  MODIFY `report_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT for table `verification`
+--
+ALTER TABLE `verification`
+  MODIFY `verification_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `properties`
+--
+ALTER TABLE `properties`
+  ADD CONSTRAINT `properties_ibfk_1` FOREIGN KEY (`landlord_id`) REFERENCES `landlords` (`landlord_id`);
+
+--
+-- Constraints for table `reports`
+--
+ALTER TABLE `reports`
+  ADD CONSTRAINT `reports_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`);
+
+--
+-- Constraints for table `verification`
+--
+ALTER TABLE `verification`
+  ADD CONSTRAINT `verification_ibfk_1` FOREIGN KEY (`property_id`) REFERENCES `properties` (`property_id`),
+  ADD CONSTRAINT `verification_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
